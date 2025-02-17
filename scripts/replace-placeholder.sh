304096be2102f97563f91966d0a33a446f567c38
Leandro Code <154637101+leandro-codee@users.noreply.github.com>
2025-02-17 01:20:44 -0300

    Feature/update workflows (#12)
    * updated workflows
    
    * some

--- scripts/replace-placeholder.sh
+++ scripts/replace-placeholder.sh
@@ -1,16 +1,39 @@
-FROM=$1
-TO=$2
+#!/bin/bash
 
-if [ "${FROM}" = "${TO}" ]; then
-    echo "Nothing to replace, the value is already set to ${TO}."
+# Function to replace values
+replace_value() {
+    FROM=$1
+    TO=$2
+    
+    if [ "${FROM}" = "${TO}" ]; then
+        echo "Nothing to replace, the value is already set to ${TO}."
+        return
+    fi
 
-    exit 0
+    echo "Replacing all statically built instances of $FROM with $TO."
+
+    find apps/web/.next/ apps/web/public -type f |
+    while read file; do
+        # Use perl instead of sed for better handling of special characters
+        perl -pi -e "s|$FROM|$TO|g" "$file"
+    done
+}
+
+# Replace URL
+replace_value "$1" "$2"
+
+# Replace App Name
+DEFAULT_APP_NAME="Cal.com"
+NEW_APP_NAME="${NEXT_PUBLIC_APP_NAME:-$DEFAULT_APP_NAME}"
+
+if [ "$DEFAULT_APP_NAME" != "$NEW_APP_NAME" ]; then
+    replace_value "$DEFAULT_APP_NAME" "$NEW_APP_NAME"
 fi
 
-# Only peform action if $FROM and $TO are different.
-echo "Replacing all statically built instances of $FROM with $TO."
+# Replace Company Name
+DEFAULT_COMPANY_NAME="Cal.com, Inc."
+NEW_COMPANY_NAME="${NEXT_PUBLIC_COMPANY_NAME:-$DEFAULT_COMPANY_NAME}"
 
-find apps/web/.next/ apps/web/public -type f |
-while read file; do
-    sed -i "s|$FROM|$TO|g" "$file"
-done
+if [ "$DEFAULT_COMPANY_NAME" != "$NEW_COMPANY_NAME" ]; then
+    replace_value "$DEFAULT_COMPANY_NAME" "$NEW_COMPANY_NAME"
+fi
